5e4ce760616987c912bafe716bd8b535a9d82fa4
Suganya <suganya.au@gmail.com>
2023-02-20 08:59:54 +0000

    Added joblayout.html for rendering the jobs under Open Positions title and did step3 - Render dynamic data using Jinja template tags

--- main.py
+++ main.py
@@ -11,15 +11,42 @@
 #3. Style the page using CSS classes, properties, and values
 #4. Use the Bootstrap framework for faster development
 
+#Lesson 2 - Flask Templates and Cloud Deployment
+#Step 3 - Dynamic Data & Cloud Deployment
+#1. Render dynamic data using Jinja template tags
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
+open_positions = [{
+  'id': 1,
+  'title': 'Data Scientist',
+  'location': 'Bangalore, India',
+  'salary': '1,20,000'
+}, {
+  'id': 2,
+  'title': 'Data Analyst',
+  'location': 'Delhi, India',
+  'salary': '2,20,000'
+}, {
+  'id': 3,
+  'title': 'Full Stack Developer',
+  'location': 'Bangalore, India',
+  'salary': '12,20,000'
+}, {
+  'id': 4,
+  'title': 'Backend Engineer',
+  'location': 'Remote',
+}]
 
 
 @app.route('/')
 def hello_world():
   #return "hello world"
-  return render_template("home.html")
+  return render_template("home.html",
+                         openpositions=open_positions,
+                         company_name="Jovian")
 
 
 if __name__ == '__main__':

aa0d6444366a6180059c882fefedfe93c23703ab
Suganya <suganya.au@gmail.com>
2023-02-21 04:24:40 +0000

    Finished L2: Step3 and Step4: Flask Templates and Cloud Deployment

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Lesson1- Step1 and Step 2
+# Lesson1- Building Web Pages with HTML & CSS [Step1 and Step 2]
 #Step 1: Project Setup & Flask Basics
 #1. Create a project on GitHub
 #2. Open up the project on Replit
@@ -11,12 +11,18 @@
 #3. Style the page using CSS classes, properties, and values
 #4. Use the Bootstrap framework for faster development
 
-#Lesson 2 - Flask Templates and Cloud Deployment
+#Lesson 2 - Flask Templates and Cloud Deployment [Step3 and Step 4]
 #Step 3 - Dynamic Data & Cloud Deployment
 #1. Render dynamic data using Jinja template tags
 #2. Add an API route to return JSON using jsonify [https://web-development-with-python-and-flask.suganyaram.repl.co/api/openpositions]
 #3. Deploy the project to Render.com [https://web-development-with-python-and-flask.onrender.com/]
 #4. Connect a domain with render deployment
+
+#Step4 - Functional and Aesthetic Improvements
+#1. Add a Navbar and Footer from Bootstrap
+#2. Add mailto: links for the buttons
+#3. Make the website mobile-friendly(responsive)
+#4. Refactor templates into reusable components
 
 from flask import Flask, render_template, jsonify
 
